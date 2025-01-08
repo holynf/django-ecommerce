c4dfc708cde3bdd0d6e55e802764ecbc167a064b
Soheil <abolfazlsf2112@gmail.com>
2025-01-08 09:03:49 +0330

    add view to add and delete comment

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product_all'),
     path('products/<slug:slug>/', views.ProductDetail.as_view(), name='product_detail'),
     path('products/<slug:slug>/comments/', views.ProductCommentView.as_view(), name='comment-create'),
-    path('products/<slug:slug>/comments/<int:comment_id>/', views.ProductCommentView.as_view(), name='comment-delete'),
+    path('products/<int:product_id>/comments/<int:comment_id>/', views.ProductCommentView.as_view(), name='comment-delete'),
     path('categories/', views.CategoryList.as_view(), name='category_list')
 ]
